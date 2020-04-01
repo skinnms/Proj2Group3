85b8dc1736d2fa0fc862bd111d2a6f8d5f521d5d
loupacmatt <loupacmatt@gmail.com>
2020-04-01 12:50:57 -0500

    final push i hope

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 from flask import Flask, g, render_template
 from contextlib import closing
 
-# list of tables from nba.db
+## list of tables from nba.db
 # nba_2016_2017_100
 # nba_2017_att_val
 # nba_2017_att_val_elo
@@ -48,30 +48,58 @@ def ppt():
 def vis1():
     # Read sqlite query results into a pandas DataFrame
     con = sqlite3.connect("nba.db")
-    df = pd.read_sql_query("SELECT * from nba_2016_2017_100", con)
+    df = pd.read_sql_query("SELECT * from nba_2017_nba_players_with_salary", con)
     # verify that result of SQL query is stored in the dataframe
     print(df.to_json())
     con.close()
     # define the varibles and render to page
-    labels = df["PLAYER_NAME"].values.tolist()  # x axis
-    values = df["MIN"].values.tolist()
-    data2 = df["W_PCT"].values.tolist()
-    return render_template("vis1.html", labels=labels, values=values, data2=data2)
+    labels = df["SALARY_MILLIONS"].values.tolist()
+    values = df["POINTS"].values.tolist()
+    return render_template("vis1.html", labels=labels, values=values)
 
 
 @app.route("/vis2.html", methods=["GET"])
 def vis2():
-    return render_template("vis2.html")
+    # Read sqlite query results into a pandas DataFrame
+    con = sqlite3.connect("nba.db")
+    df = pd.read_sql_query("SELECT * from nba_2017_att_val", con)
+    # verify that result of SQL query is stored in the dataframe
+    print(df.to_json())
+    con.close()
+    # define the variables and render to page
+    values = df["PCT"].values.tolist()  # x axis
+    labels = df["TEAM"].values.tolist()
+    data2 = df["VALUE_MILLIONS"].values.tolist()
+    return render_template("vis2.html", labels=labels, values=values, data2=data2)
 
 
 @app.route("/vis3.html", methods=["GET"])
 def vi3():
-    return render_template("vis3.html")
+    con = sqlite3.connect("nba.db")
+    df = pd.read_sql_query(
+        "SELECT CASE WHEN SALARY_MILLIONS < 10 THEN 'Under 10 Million' WHEN SALARY_MILLIONS >= 10 AND SALARY_MILLIONS < 20 THEN '10-20 Million' WHEN SALARY_MILLIONS >= 20 AND SALARY_MILLIONS < 30 THEN '20-30 Million' ELSE 'Over 30 Million' END AS Salary_Bucket, SUM(CASE WHEN TWITTER_FAVORITE_COUNT = '' OR TWITTER_FAVORITE_COUNT IS NULL THEN 0 ELSE TWITTER_FAVORITE_COUNT END) AS TWITTER_FAVORITE_COUNT from nba_2017_players_with_salary_wiki_twitter GROUP BY CASE WHEN SALARY_MILLIONS < 10 THEN 'Under 10 Million' WHEN SALARY_MILLIONS >= 10 AND SALARY_MILLIONS < 20 THEN '10-20 Million' WHEN SALARY_MILLIONS >= 20 AND SALARY_MILLIONS < 30 THEN '20-30 Million' ELSE 'Over 30 Million' END",
+        con,
+    )
+    # verify that result of SQL query is stored in the dataframe
+    print(df.to_json())
+    con.close()
+    # define the varibles and render to page
+    labels = df["Salary_Bucket"].values.tolist()  # x axis
+    values = df["TWITTER_FAVORITE_COUNT"].values.tolist()
+    return render_template("vis3.html", labels=labels, values=values)
 
 
 @app.route("/vis4.html", methods=["GET"])
 def vis4():
-    return render_template("vis4.html")
+    con = sqlite3.connect("nba.db")
+    df = pd.read_sql_query("SELECT TEAM, AVG(AGE) from nba_2017_pie GROUP BY TEAM", con)
+    # verify that result of SQL query is stored in the dataframe
+    print(df.to_json())
+    con.close()
+    # define the varibles and render to page
+    labels = df["TEAM"].values.tolist()  # x axis
+    values = df["AVG(AGE)"].values.tolist()
+    return render_template("vis4.html", labels=labels, values=values)
 
 
 if __name__ == "__main__":
